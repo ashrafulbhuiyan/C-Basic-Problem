af7a6f5c17a59e4849efef7f1ab475527da28680
Ashraful Hasan Bhuiyan <54420966+ashrafulbhuiyan@users.noreply.github.com>
2024-10-15 00:02:39 +0600

    Create right_half_pyramid_pattern.c

---
+++ right_half_pyramid_pattern.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i,j, row = 5;
+    
+    for(i=1; i<=row; i++) {
+        for(j=0; j<i; j++) {
+            printf("* ");
+        }
+        
+        printf("\n");
+    }
+        
+
+    return 0;
+}
